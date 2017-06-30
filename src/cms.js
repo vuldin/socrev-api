a9789552c41f5c4ae45cc19b79b6c021ff8f00a1
Joshua Purcell <vuldin@gmail.com>
2017-06-30 12:58:45 -0700

    in-memory cache with 20 min life

--- src/cms.js
+++ src/cms.js
@@ -1,10 +1,30 @@
 const WPAPI = require('wpapi')
 const request = require('superagent')
 const { JSDOM } = require('jsdom')
+const mcache = require('memory-cache')
 const CircularJSON = require('circular-json')
 const store = require('./store')
 
 const { cmsApiUrl, cmsApiUser, cmsApiPassword } = store
+const cachelife = 60 * 20 // 20 min cache
+const cache = duration => {
+  return (req, res, next) => {
+    let key = '__express__' + req.originalUrl || req.url
+    let cachedBody = mcache.get(key)
+    if (cachedBody) {
+      res.json(cachedBody)
+      return
+    } else {
+      res.sendResponse = res.json
+      res.json = body => {
+        mcache.put(key, body, duration * 1000)
+        res.sendResponse(body)
+      }
+      next()
+    }
+  }
+}
+
 module.exports = (app, checkJwt, checkScopes) => {
   WPAPI.discover(cmsApiUrl).then(wp => {
     wp.auth({
@@ -12,7 +32,7 @@ module.exports = (app, checkJwt, checkScopes) => {
       password: cmsApiPassword,
       auth: true
     })
-    app.get('/posts', async (req, res) => {
+    app.get('/posts', cache(cachelife), async (req, res) => {
       let page = req.query.page
       try {
         let [sticky, posts] = await Promise.all([
@@ -25,7 +45,7 @@ module.exports = (app, checkJwt, checkScopes) => {
         res.status(404).send('error from wordpress')
       }
     })
-    app.get('/posts/:id', async (req, res) => {
+    app.get('/posts/:id', cache(cachelife), async (req, res) => {
       try {
         let post = await wp.posts().id(req.params.id)
         res.json(post)
@@ -33,6 +53,9 @@ module.exports = (app, checkJwt, checkScopes) => {
         res.status(404).send('error from wordpress')
       }
     })
-    app.get('/categories', async (req, res) => {})
+    app.get('/clear', (req, res) => {
+      mcache.clear()
+      res.send('cache cleared')
+    })
   })
 }

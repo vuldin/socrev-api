f49ced98106f76b6478566cb4dadcf16c3f86e06
Joshua Purcell <vuldin@gmail.com>
2017-07-29 21:10:51 -0700

    add count parameter to endpoint

---
+++ TASKS.md
@@ -0,0 +1,4 @@
+- COMPLETE automate API cache reloading
+- COMPLETE return remaining posts with each request
+- cleanup api console log
+- add sitemap.xml https://www.npmjs.com/package/sitemap
--- src/cache.js
+++ src/cache.js
@@ -15,9 +15,12 @@ module.exports = {
         const slug = req.params.slug
         const page = parseInt(req.query.page)
         const cat = parseInt(req.query.category)
+        const isCount = req.query.count === 'true'
         let result = mcache.get('posts')
+        let left = result.length
         if (slug) {
           result = result.find(d => d.slug === slug)
+          left = 0
           //if (result) console.log(`cached post ${result.slug}`)
         } else {
           //console.log(`cached posts length: ${result.length}`)
@@ -28,6 +31,7 @@ module.exports = {
                 isOfCategory = true
               return isOfCategory
             })
+            left = result.length
           }
           if (page) {
             //console.log(`page ${page}`)
@@ -36,16 +40,23 @@ module.exports = {
             // and return 12 for a page1 category/search request, and all other pages
             let firstPageSize = 13
             if (cat) firstPageSize = 12
+            left = left - firstPageSize
             if (page === 1) {
               result = result.slice(0, 0 + firstPageSize)
             } else {
               const offset = firstPageSize + 12 * (page - 2)
               result = result.slice(offset, offset + 12)
+              left = left - 12 * (page - 2) - result.length
             }
           }
         }
         //result = result.slice(0, 100) // limit return to 100 posts
-        res.json(result)
+        if (left < 0) left = 0
+        if (isCount)
+          res.json({
+            postsLeft: left
+          })
+        else res.json(result)
       }
       if (url.includes('categories')) {
         const cats = mcache.get('cats')
